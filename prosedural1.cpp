1069e44a141721d4f8edaa0dac492a8a255f025b
RidwanC134 <ridwanhh10gmail.com>
2023-03-06 11:46:58 +0700

    first commit

---
+++ prosedural1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Begin
+    // Numeric Luas, p, l
+    double Luas,p,l;
+    // display 'Masukan panjangnya = '
+    cout << "Masukan Panjangnya = ";
+    // Accept p
+    cin >> p;
+    // display 'Masukan lebarnya ='
+    cout << "Masukan lebarnya = ";
+    // Accept l
+    cin >> l;
+    // compute Luas = p*l
+    Luas = p*l;
+    // display "Luasnya = +Luas"
+    cout << "Luasnya = " << Luas;
+    // End
+}
